a6f4c5c40774984560591a2ef26d770f780b3243
Spex2017 <bilalnaz.77@gmail.com>
2017-02-26 15:11:23 +0100

    Add files via upload

---
+++ Reseau_n.c
@@ -0,0 +1,119 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Reseau.h"
+//#include "Chaine.h"
+
+//typedef struct noeud Noeud;
+
+Noeud* initialiser_noeud(int num, double x, double y){
+	Noeud* n=(Noeud*)malloc(sizeof(Noeud));
+	if(n==NULL)return NULL;
+
+	n->num=num;
+	n->x=x;
+	n->y=y;
+	n->voisins=NULL;
+			
+	return n;
+}
+
+CellNoeud* initialiser_cellNoeud(Noeud* N){
+	CellNoeud* cn=(CellNoeud*)malloc(sizeof(CellNoeud));
+	if(cn==NULL)return NULL;
+
+	cn->nd=N;
+	cn->suiv=NULL;
+			
+	return cn;
+}
+
+CellCommodite* initialiser_cellCommodite(Noeud* N1, Noeud* N2){
+	CellCommodite* cc=(CellCommodite*)malloc(sizeof(CellCommodite));
+	if(cc==NULL)return NULL;
+
+	cc->extrA=N1;
+	cc->extrB=N2;
+	cc->suiv=NULL;
+			
+	return cc;
+}
+
+Reseau* initialiser_reseau(int nbNoeuds, int gamma)
+{
+	Reseau* r=(Reseau*)malloc(sizeof(Reseau));
+	if(r==NULL)return NULL;
+
+	r->nbNoeuds=nbNoeuds;
+	r->gamma=gamma;
+	r->noeuds=NULL;
+	r->commodites=NULL;
+
+	return r;
+}
+
+//insérer un Noeud dans un CellNoeud
+void inserer_CN_N(CellNoeud* CN, Noeud* N)
+{
+	if(CN->nd==NULL)
+	{
+		CN->nd=N;
+		
+	}
+	else{
+		CellNoeud* cour=CN;
+		
+		while(cour->suiv){
+			cour=cour->suiv;
+		}
+		cour->nd->voisins->suiv=cour;
+		cour->nd=N;
+	}
+
+}
+
+void inserer_lCN_CN(CellNoeud* lCN, CellNoeud* CN)
+{
+	if(lCN==NULL)
+	{
+		lCN=CN;	
+	}
+	else
+	{
+		N->voisins->suiv=CN;
+		CN->nd=N;
+	}
+}
+
+void inserer_R_CN(Reseau* R, CellNoeud* CN){
+	
+	if(R->noeuds==NULL)R->noeuds=CN;
+	else{
+		CN->suiv=noeuds;
+		noeuds=CN;
+	}
+}
+
+Noeud* rechercheCreeNoeudListe(Reseau *R, double x, double y){
+	CellNoeud* cour=R->noeuds;
+
+	while(cour){
+		if(cour->nd->x==x && cour->nd->y==y){
+			return cour->nd;
+		}
+
+	cour=cour->suivant;
+	}
+
+	Noeud* N=initialiser_noeud(R->nbNoeuds+1, x, y);
+	CellNoeud* CN=initialiser_cellNoeud(N);
+	inserer_R_CN(R,CN);
+	
+	R->nbNoeuds++;
+
+	return N;
+}
+
+Reseau* reconstitueReseauListe(Chaines* C){
+	int nbPointsTotal=0;	
+	
+	
