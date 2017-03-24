58de81711bafb309e09dfc0579a82a313a993777
Chris KASA <chris.kasa_sombo@etu.upmc.fr>
2017-03-24 14:51:52 +0100

    Add files via upload

---
+++ exo5.c
@@ -0,0 +1,205 @@
+
+
+/* initialise l'ArbreQuat */
+
+ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY)
+{
+	ArbreQuat *AB = (ArbreQuat*)malloc(sizeof(ArbreQuat);
+	if(AB==NULL) return NULL;
+
+	AB->xc = xc;
+	AB->yc = yc;
+	AB->coteX = coteX;
+	AB->coteY = coteY;
+		
+	AB->noeud = NULL;
+	AB->se = NULL; 
+	AB->so = NULL;
+	AB->no = NULL;
+	AB->ne = NULL;
+
+	return AB;
+}
+
+
+/* les coordonnée minimales et maximales des points du Reseau */
+
+void chaineCoordMinMax(Chaines* C, double* xmin, double* ymin, double* xmax, double* ymax)
+{
+	minimales(C, xmin, ymin);
+	maximales(C, xmax, ymax);	
+
+}
+
+/* les coordonées minimales */
+
+void minimales(Chaine *C, double *xmin, double *ymin)
+{
+	CellChaine *CC = C->chaines;
+
+	CellPoint *CP = CC->points;
+
+	double X = CP->x;
+	double Y = CP->y;
+
+	CP = CP->suiv;
+
+	while(CC){
+	
+		while(CP){
+	
+			if(CP->x < X) X = CP->x;
+			if(CP->y < Y) Y = CP->y;
+
+			CP = CP->suiv;
+		}
+	
+		CC = CC->suiv;
+	}
+
+	*xmin = X;
+	*ymin = Y;
+
+}
+
+/* les coordonées maximales */
+
+void maximales(Chaine *C, double *xmax, double *ymax)
+{
+	CellChaine *CC = C->chaines;
+
+	CellPoint *CP = CC->points;
+
+	double X = CP->x;
+	double Y = CP->y;
+
+	CP = CP->suiv;
+
+	while(CC){
+	
+		while(CP){
+	
+			if(CP->x > X) X = CP->x;
+			if(CP->y > Y) Y = CP->y;
+
+			CP = CP->suiv;
+		}
+	
+		CC = CC->suiv;
+	}
+
+	*xmax = X;
+	*ymax = Y;
+
+}
+
+
+/* insertion : elle determine la region à insérer l'arbre dans un arbre  */
+
+ArbreQuat* inserer_AB_AB(ArbreQuat *parent, ArbreQuat *a)
+{
+	double x = a->noeud->x;
+	double y = a->noeud->y;
+
+	double xc = parent->xc;
+	double yc = parent->yc;
+
+	ArbreQuat *O = NULL;
+
+	if(x>=xc && y>=yc){
+		
+	 	if(parent->ne==NULL){
+
+			 parent->ne = a; // Nord-Est
+	 		 return parent;
+
+		}
+
+		insererNoeudArbre(a->noeud, O, parent->ne);	
+
+	}
+
+	if(x<xc && y>=yc){
+
+		 if(parent->no==NULL){
+	
+			parent->no = a; // Nord-Ouest
+			return parent;
+		 }
+		
+		insererNoeudArbre(a->noeud, O, parent->no);
+	}
+	
+	if(x>=xc && y<yc){
+
+		if(parent->se==NULL){
+ 			parent->se = a; // Sud-Est
+		 	return parent;
+		 }
+
+		insererNoeudArbre(a->noeud, O, parent->se);
+		
+	}
+
+	if(x<xc && y<yc){
+
+		 if(parent->so==NULL){
+
+			parent->so = a; // Sud-Ouest
+		 	return parent;
+		 }
+		
+		insererNoeudArbre(a->noeud, O, parent->so);
+	}
+}
+
+
+
+{
+
+	if(a==NULL){
+		
+		double coteX = parent->coteX - parent->xc;
+		double coteY = parent->coteY - parent->yc;
+
+		double xc = coteX/2;
+		double yc = coteY/2;
+
+		ArbreQuat* a = creerArbreQuat(xc, yc, coteX,coteY);
+		a->noeud = n;
+
+		return inserer_AB_AB(parent, a);
+
+
+	}
+
+	if( a->noeud != NULL){
+
+		ArbreQuat *b = NULL;
+		ArbreQuat *c = NULL;
+
+		Noeud *N = a->noeud;
+				
+		a->noeud == NULL;
+
+		a = insererNoeudArbre(n, b, a);
+
+		return insererNoeudArbre(N, c, a);
+	}
+
+	if( a!=NULL && a->noeud == NULL){
+
+		ArbreQuat *b = NULL;
+
+		return insererNoeudArbre(n, b, a);
+
+	}
+
+
+}
+
+
+
+
+
+
---
+++ exo5.h
@@ -0,0 +1,46 @@
+#ifndef EXO5_H
+#define EXO5_H
+
+#include "Reseau.h"
+
+
+/* arbre quaternaire contenant les noeuds du reseau */
+
+typedef struct arbreQuat{
+
+	double xc , yc; /* Coordonnees du centre de la cellule */
+	double coteX;  /* Longueur de la cellule */
+	double coteY;  /* Hauteur de la cellule */
+	Noeud* noeud;  /* Pointeur vers le noeud du reseau */
+	struct arbreQuat *so; /* Sous-arbre sud-ouest, pour x < xc et y < yc */
+	struct arbreQuat *se; /* Sous-arbre sud-est, pour x >= xc et y < yc */
+	struct arbreQuat *no; /* Sous-arbre nord-ouest, pour x < xc et y >= yc */
+	struct arbreQuat *ne; /* Sous-arbre nord-est, pour x >= xc et y >= yc */
+
+
+} ArbreQuat;
+
+/* initialise l'ArbreQuat */
+
+ArbreQuat* creerArbreQuat(double xc, double yc, double coteX, double coteY)
+
+/* les coordonnée minimales des points du Reseau */
+
+void chaineCoordMinMax(Chaines* C, double* xmin, double* ymin, double* xmax, double* ymax);
+
+/* les coordonées minimales */
+
+void minimales(Chaine *C, double *xmin, double *ymin);
+
+/* les coordonées maximales */
+
+void maximales(Chaine *C, double *xmax, double *ymax);
+
+/* insertion : elle determine la region à insérer l'arbre dans un arbre  */
+
+ArbreQuat* inserer_AB_AB(ArbreQuat *parent, ArbreQuat *a);
+
+/* inserer un noeud dans un ArbreQuat */
+ArbreQuat* insererNoeudArbre(Noeud* n, ArbreQuat* a, ArbreQuat* parent);
+
+#endif
